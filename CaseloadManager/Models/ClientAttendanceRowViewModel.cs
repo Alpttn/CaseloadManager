d4ae87e9454c854f6f76d6762db4a81d44cca277
Allison Patton <allison.tpttn@gmail.com>
2020-01-09 12:10:16 -0600

    changed name of sessions had, some flexbox improvements

--- CaseloadManager/Models/ClientAttendanceRowViewModel.cs
+++ CaseloadManager/Models/ClientAttendanceRowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,7 @@ namespace CaseloadManager.Models
     public class ClientAttendanceRowViewModel
     {
         public Client Client { get; set; }
-
+        [Display(Name = "Sessions Complete")]
         public int SessionsHad { get; set; }
     }
 }
